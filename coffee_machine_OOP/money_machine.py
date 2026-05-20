124cae29b990b477d6dde7be02da8347e9717acf
Jashanpreet singh <jashanpreetsinghj123@gmail.com>
2026-05-20 18:31:13 +0530

    all files are ready to be pushed

---
+++ coffee_machine_OOP/money_machine.py
@@ -0,0 +1,12 @@
+class money_control:
+    def __init__(self,recived_money,cost,profittes):
+        self.recived_money = recived_money
+        self.cost = cost
+        self.profittes = profittes
+        
+    def proffite_report(self):
+        print(f"Money: ${self.profittes}")
+        
+    
+        
+        
