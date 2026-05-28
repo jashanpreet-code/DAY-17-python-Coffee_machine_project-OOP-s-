de1c5531caf47906cc01f236d6150ec3c2f8af9e
jashanpreet <jashanpreetsinghj123@gmail.com>
2026-05-28 14:05:57 +0530

    Refactor money_control class to inherit from cash_system and implement initial cash handling methods

--- coffee_machine_OOP/money_machine.py
+++ coffee_machine_OOP/money_machine.py
@@ -1,4 +1,12 @@
-class money_control:
+
+class cash_system:
+    def __init__(self,dimes,pennies,nickles,quarters):
+        self.dimes = 0.10
+        self.pennies = 0.01
+        self.nickles = 0.05
+        self.quarters = 0.25
+    
+class money_control(cash_system):
     def __init__(self,recived_money,cost,profittes):
         self.recived_money = recived_money
         self.cost = cost
@@ -7,9 +15,7 @@ class money_control:
     def proffite_report(self):
         print(f"Money: ${self.profittes}")
         
-    def payment_check:
-        
-        
-    
+    def payment_check(self):
+        pass
         
         
